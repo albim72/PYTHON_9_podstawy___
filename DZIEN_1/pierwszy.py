be90eda6bd4c2658196db6b8fd27cfba55897928
albim72 <marcin.albiniak72@gmail.com>
2024-09-02 12:17:24 +0200

    Update pierwszy.py

--- DZIEN_1/pierwszy.py
+++ DZIEN_1/pierwszy.py
@@ -17,4 +17,16 @@ print(f"liczba zmiennoprzecinkowa: {liczba_zm}, zmienna typu: {type(liczba_zm)}"
 liczba_zesp = 2 + 3j
 print(f"liczba zespolona: {liczba_zesp}, zmienna typu: {type(liczba_zesp)}")
 
+#wartośc logiczna (bool)
+wart_logiczna = True
+print(f"wartośc logiczna: {wart_logiczna}, zmienna typu: {type(wart_logiczna)}")
+
+#typ bajtowy (Bytes)
+typ_bajt = b"Witaj typie Bajt!"
+print(f"typ byte: {typ_bajt}, w typie {type(typ_bajt)}")
+
+#przykład modyfikacji bytearray
+typ_bytearray = bytearray(b'Witaj ByteArray!')
+typ_bytearray[0] = 104 #kod ASCII litery h
+print(f"zmodyfikowany type bytearray: {typ_bytearray}")
 
