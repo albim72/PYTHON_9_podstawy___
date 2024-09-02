7b0bcd46ef838a4a7fc464eb75dce68dcf22da9e
albim72 <marcin.albiniak72@gmail.com>
2024-09-02 12:02:53 +0200

    Update pierwszy.py

--- DZIEN_1/pierwszy.py
+++ DZIEN_1/pierwszy.py
@@ -8,3 +8,7 @@ print("liczba zmiennoprzecinkowa:",liczba_calk,"zmienna typu:",type(liczba_calk)
 opis = "f-string"
 
 print(f"struktura ekstrapolacji ciągów str w postaci {opis}")
+
+#liczba zmiennoprzecinkowa (float)
+liczba_zm = 19.89
+print(f"liczba zmiennoprzecinkowa: {liczba_zm}, zmienna typu: {type(liczba_zm)}")
