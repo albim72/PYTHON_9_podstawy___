ec0793d3d48249182f29f1dd41b687de5486fafe
albim72 <marcin.albiniak72@gmail.com>
2024-09-02 12:07:27 +0200

    Update pierwszy.py

--- DZIEN_1/pierwszy.py
+++ DZIEN_1/pierwszy.py
@@ -4,7 +4,7 @@ print(tekst)
 
 #liczba calkowita (int)
 liczba_calk = 10
-print("liczba zmiennoprzecinkowa:",liczba_calk,"zmienna typu:",type(liczba_calk))
+print("liczba stałoprzecinkowa:",liczba_calk,"zmienna typu:",type(liczba_calk))
 opis = "f-string"
 
 print(f"struktura ekstrapolacji ciągów str w postaci {opis}")
@@ -12,3 +12,9 @@ print(f"struktura ekstrapolacji ciągów str w postaci {opis}")
 #liczba zmiennoprzecinkowa (float)
 liczba_zm = 19.89
 print(f"liczba zmiennoprzecinkowa: {liczba_zm}, zmienna typu: {type(liczba_zm)}")
+
+#liczba zespolona (complex)
+liczba_zesp = 2 + 3j
+print(f"liczba zespolona: {liczba_zesp}, zmienna typu: {type(liczba_zesp)}")
+
+
