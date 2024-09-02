bae037e27f6003e997d74540ab6359cc0c3ecb21
albim72 <marcin.albiniak72@gmail.com>
2024-09-02 14:42:51 +0200

    Update python_lista.py

--- DZIEN_1/python_lista.py
+++ DZIEN_1/python_lista.py
@@ -19,3 +19,11 @@ kraje.reverse() #odwrócenie kolejności w liście
 print(kraje)
 
 liczby = [2,4,56,78,-9,0,12,-45,0,98,24,51,25,29,221,-116]
+print(liczby)
+liczby.sort(reverse=True) #sortowanie odwrotne Z-A
+print(liczby)
+liczby.remove(-9) #usuwanie elementu - pierwsza wartośc od lewej strony
+print(liczby)
+
+powtorzenie = [2,3,5,6,8,3,9,6,4,3,2,5,7,4,3,5,3,6,3]
+
