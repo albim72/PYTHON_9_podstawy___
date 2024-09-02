3b13dc770233df2b9ebf9657828b177d2145036e
albim72 <marcin.albiniak72@gmail.com>
2024-09-02 11:44:25 +0200

    Create pierwszy.py

---
+++ DZIEN_1/pierwszy.py
@@ -0,0 +1,10 @@
+print("to jest wstęp do języka Python!")
+tekst = "to jest zmienna typu tesktowego -> str"
+print(tekst)
+
+#liczba calkowita (int)
+liczba_calk = 10
+print("liczba zmiennoprzecinkowa:",liczba_calk,"zmienna typu:",type(liczba_calk))
+opis = "f-string"
+
+print(f"struktura ekstrapolacji ciągów str w postaci {opis}")
