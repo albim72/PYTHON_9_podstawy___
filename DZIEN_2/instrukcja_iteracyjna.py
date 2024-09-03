0523c8d275548a7a81b6db99b35962c698b38c5a
albim72 <marcin.albiniak72@gmail.com>
2024-09-03 12:10:20 +0200

    Update instrukcja_iteracyjna.py

--- DZIEN_2/instrukcja_iteracyjna.py
+++ DZIEN_2/instrukcja_iteracyjna.py
@@ -1,8 +1,16 @@
 #przykład 1
-print("_"*50)
+print("_"*60)
 miasta = ["Warszawa","Lublin","Kraków","Gliwice","Toruń"]
 
 print(miasta)
+
 # miasto:str
 for miasto in miasta:
     print(miasto)
+
+print(miasta[3])
+#przykład 2 - pętla for z użyciem funkcji range()
+print("_"*60)
+
+for i in range(1,21,2):
+    print(f"wartośc i*3 = {i*3}")
