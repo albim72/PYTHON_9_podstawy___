7b7d656332f8801b58979ad3291248cabe2e3dc1
albim72 <marcin.albiniak72@gmail.com>
2024-09-03 12:13:23 +0200

    Update instrukcja_iteracyjna.py

--- DZIEN_2/instrukcja_iteracyjna.py
+++ DZIEN_2/instrukcja_iteracyjna.py
@@ -14,3 +14,14 @@ print("_"*60)
 
 for i in range(1,21,2):
     print(f"wartośc i*3 = {i*3}")
+
+#przykład 3 - pętla while
+print("_"*60)
+licznik = 1
+while licznik <= 6:
+    print(licznik)
+    licznik += 1
+
+# while True:
+#     print(licznik)
+#     licznik += 1
