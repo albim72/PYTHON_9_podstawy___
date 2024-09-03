152d05c8c4a112802823de873ceb9e88c27294b6
albim72 <marcin.albiniak72@gmail.com>
2024-09-03 09:52:18 +0200

    Update zbiory.py

--- DZIEN_2/zbiory.py
+++ DZIEN_2/zbiory.py
@@ -11,3 +11,34 @@ print(f"drzewa: {drzewa}")
 #dodawanie elementu
 drzewa.add("modrzew")
 print(f"drzewa: {drzewa}")
+
+#usuwanie elementu ze zbioru
+drzewa.remove("sosna")
+print(f"drzewa: {drzewa}")
+
+# drzewa.remove("buk")
+drzewa.discard("buk")
+print(f"drzewa: {drzewa}")
+
+#sprawdzenie czy element jest w zbiorze
+czy_jest = "dąb" in drzewa
+print(f"czy dab jest w zbiorze? {czy_jest}")
+
+#opercje na zbiorach: suma,przecięcie,różnica
+inne_drzewa = {"buk","jodła","topola","klon"}
+
+#suma
+suma = drzewa | inne_drzewa
+print(suma)
+
+#przecięcie -  częśc wspólna zbiorów
+przeciecie = drzewa & inne_drzewa
+print(przeciecie)
+
+#różnica zbiorów - elementy które są w zbiorze drzewa ale nie ma ich w inne_drzewa
+roznica = drzewa - inne_drzewa
+print(roznica)
+
+#różnica symetryczna - elementy które są w jednym zbiorze lub drugim ale nie ma ich w obu jednocześnie
+roz_symetryczna = drzewa ^ inne_drzewa
+print(roz_symetryczna)
