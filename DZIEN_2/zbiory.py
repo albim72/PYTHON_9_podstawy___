d3ac5a6df6893a2a03ea74787a9411d75981a6e8
albim72 <marcin.albiniak72@gmail.com>
2024-09-03 09:13:46 +0200

    Create zbiory.py

---
+++ DZIEN_2/zbiory.py
@@ -0,0 +1,13 @@
+#zbiór - set - nawias tworzący zbiór {}
+drzewa = {"dąb","sosna","klon","brzoza","topola","jesion","cyprys"}
+print(f"drzewa: {drzewa}")
+print(f"drzewa: {drzewa}")
+print(f"drzewa: {drzewa}")
+
+# for d in drzewa:
+#     print(d)
+
+#operacje na zbiorach
+#dodawanie elementu
+drzewa.add("modrzew")
+print(f"drzewa: {drzewa}")
