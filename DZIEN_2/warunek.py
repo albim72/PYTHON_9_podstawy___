70ae15d9b2aa3b3705d74f1e192eb2f83fb1d53b
albim72 <marcin.albiniak72@gmail.com>
2024-09-03 11:17:16 +0200

    Update warunek.py

--- DZIEN_2/warunek.py
+++ DZIEN_2/warunek.py
@@ -12,7 +12,24 @@ if liczba > 0:
 print("\n________ przykład 2_________")
 liczba = -5
 
-if liczba >0:
+if liczba > 0:
     print("liczba jest dodatnia")
 else:
     print("liczba jest ujemna lub równa 0")
+
+
+#przykład 3 - warunek z klauzulą else oraz elif (else if)
+
+print("\n________ przykład 3_________")
+liczba = 0
+
+if liczba > 0:
+    print("liczba jest dodatnia")
+elif liczba < 0:
+    print("liczba jest ujemna")
+else:
+    print("liczba jest równa 0")
+    
+#przykład 4 - zagnieżdzone instrukcje if
+
+print("\n________ przykład 4_________")
