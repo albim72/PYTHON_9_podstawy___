b40b210be4229dbe09ea51f5f91ac1b0aee1d345
albim72 <marcin.albiniak72@gmail.com>
2024-09-03 13:34:08 +0200

    Update funkcje_wstep.py

--- DZIEN_2/funkcje_wstep.py
+++ DZIEN_2/funkcje_wstep.py
@@ -5,3 +5,12 @@ def wypisz_dzien_dobry():
 wypisz_dzien_dobry()
 wypisz_dzien_dobry()
 wypisz_dzien_dobry()
+
+#przykład 2 - podstawowa funkcja nbez aargumentów i ze zwracaniem wartości
+
+def pisz_kolor():
+    return "kolor podstawowy -> biały!"
+
+print(pisz_kolor())
+powitanie = pisz_kolor() + " Drugi kolor -> czarny..."
+print(powitanie)
