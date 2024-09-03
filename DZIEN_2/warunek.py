2e457b4b63a86085bc92990b8acc883dc24e95af
albim72 <marcin.albiniak72@gmail.com>
2024-09-03 11:13:45 +0200

    Update warunek.py

--- DZIEN_2/warunek.py
+++ DZIEN_2/warunek.py
@@ -7,3 +7,12 @@ liczba = 10
 if liczba > 0:
     print("liczba jest dodatnia")
 
+#przykład 2 - warunek z klauzulą else
+
+print("\n________ przykład 2_________")
+liczba = -5
+
+if liczba >0:
+    print("liczba jest dodatnia")
+else:
+    print("liczba jest ujemna lub równa 0")
