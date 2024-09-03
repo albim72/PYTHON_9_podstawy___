0680c9e261f3d62d5428667d5acb502e16728d5f
albim72 <marcin.albiniak72@gmail.com>
2024-09-03 13:29:26 +0200

    Create funkcje_wstep.py

---
+++ DZIEN_2/funkcje_wstep.py
@@ -0,0 +1,7 @@
+#przykład 1 - podstawowa funkcja nbez aargumentów i bez zwracania wartości
+def wypisz_dzien_dobry():
+    print("Powitanie - Hej to ja - funkcja!")
+
+wypisz_dzien_dobry()
+wypisz_dzien_dobry()
+wypisz_dzien_dobry()
