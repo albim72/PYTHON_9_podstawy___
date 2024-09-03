f0efd5d0371c2d74ee8454a9169ae1b5267e6572
albim72 <marcin.albiniak72@gmail.com>
2024-09-03 10:22:01 +0200

    Create slownik_miasta.py

---
+++ DZIEN_2/slownik_miasta.py
@@ -0,0 +1,40 @@
+#słownik
+pojazd = {
+    "marka":"Ford",
+    "model":"Mustang",
+    "rok":2019,
+    "kolor":"czarny",
+    453:"pb98"
+}
+
+print(pojazd)
+print(pojazd["model"])
+print(pojazd[453])
+
+miasto = {
+    "Warszawa":{
+        "województwo":"mazowieckie",
+        "populacja":1890340,
+        "powierzchnia":517.24
+    },
+"Kraków":{
+        "województwo":"małopolskie",
+        "populacja":780000,
+        "powierzchnia":326.86
+    },
+"Łódź":{
+        "województwo":"łódzkie",
+        "populacja":670100,
+        "powierzchnia":293.25
+    },
+"Wrocław":{
+        "województwo":"dolnośląskie",
+        "populacja":643782,
+        "powierzchnia":292.82
+    },
+"Gdańsk":{
+        "województwo":"pomorskie",
+        "populacja":470900,
+        "powierzchnia":262
+    }
+}
