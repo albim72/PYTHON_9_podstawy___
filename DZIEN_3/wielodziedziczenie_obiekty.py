ca33b755b44bb2d50d21cbc401a2ef51107d4a9f
albim72 <marcin.albiniak72@gmail.com>
2024-09-04 11:36:58 +0200

    Update wielodziedziczenie_obiekty.py

--- DZIEN_3/wielodziedziczenie_obiekty.py
+++ DZIEN_3/wielodziedziczenie_obiekty.py
@@ -1,7 +1,7 @@
 class LadowyPojazd:
     def __init__(self,liczba_kol):
         self.liczna_kol = liczba_kol
-        
+
     def jedz(self):
         return f"jedzie po lądzie na {self.liczna_kol} kołach"
 
@@ -12,3 +12,15 @@ class WodnyPojazd:
 
     def plywaj(self):
         return f"pływa po wodzie używając {self.typ_silnika}."
+
+class Amfibia(LadowyPojazd,WodnyPojazd):
+    def __init__(self, liczba_kol, typ_silnika):
+        LadowyPojazd.__init__(self,liczba_kol)
+        WodnyPojazd.__init__(self,typ_silnika)
+
+    def przemieszczaj_sie(self):
+        return f"{self.jedz()}, {self.plywaj()}"
+
+#przykładowe użycie
+amfibia = Amfibia(4,"silnika spalinowego")
+print(amfibia.przemieszczaj_sie())
