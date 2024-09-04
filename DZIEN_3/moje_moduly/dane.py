dfb3ea4e9bd6b2d198fbf74bd0964aaa8e1a2a8a
albim72 <marcin.albiniak72@gmail.com>
2024-09-04 13:52:22 +0200

    Create dane.py

---
+++ DZIEN_3/moje_moduly/dane.py
@@ -0,0 +1,10 @@
+nrfilii = [2,56,17,8,39,49,88,106]
+
+book = {
+    "tytuł":"Wiedźmin",
+    "autor":"Andrzej Sapkowski",
+    "cena":43,
+    "wydanie":7,
+    "liczba stron":342,
+    "nr filii":nrfilii
+}
