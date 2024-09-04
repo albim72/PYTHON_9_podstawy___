cee9885a17b1525f6333158f60bded5f636fea23
albim72 <marcin.albiniak72@gmail.com>
2024-09-04 15:55:04 +0200

    Update main.py

--- DZIEN_3/figury_geometryczne/main.py
+++ DZIEN_3/figury_geometryczne/main.py
@@ -1,6 +1,7 @@
 from prostokat import Prostokat
 from trojkat import Trojkat
 from trapez import Trapez
+from kolo import Kolo
 
 pr = Prostokat(4.4,8.9)
 print(f"pole figury {pr.__class__.__name__} wynosi: {pr.policz_pole():.2f} cm2")
@@ -10,4 +11,7 @@ print(f"pole figury {tr.__class__.__name__} wynosi: {tr.policz_pole():.2f} cm2")
 
 trp = Trapez(10,6.8,5.2)
 print(f"pole figury {trp.__class__.__name__} wynosi: {trp.policz_pole():.2f} cm2")
+
+kl = Kolo(5.5)
+print(f"pole figury {kl.__class__.__name__} wynosi: {kl.policz_pole():.2f} cm2")
 
