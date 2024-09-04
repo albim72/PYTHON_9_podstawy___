dc566cb73389ea7dcd6550aa450641b0c5b853e3
albim72 <marcin.albiniak72@gmail.com>
2024-09-04 14:10:38 +0200

    Update bfunkcje.py

--- DZIEN_3/moje_moduly/funkcja/bfunkcje.py
+++ DZIEN_3/moje_moduly/funkcja/bfunkcje.py
@@ -1,9 +1,9 @@
 def czytaj_liste(lista):
-    print(f"___ lista {lista.__name__} ___")
+    print(f"___ lista {lista} ___")
     for i,j in enumerate(lista):
         print(f"element listy nr {i+1} -> wartośc: {j}")
-        
+
 def czytaj_slownik(slownik):
-    print(f"___ słownik {slownik.__name__} ___")
+    print(f"___ słownik {slownik} ___")
     for x,y in slownik.items():
         print(f"klucz: {x} -> wartośc: {y}")
