467174aa26cbc3830c0ffb6a8e543b6f51f9c2c2
albim72 <marcin.albiniak72@gmail.com>
2024-09-04 14:11:06 +0200

    Update main.py

--- DZIEN_3/moje_moduly/main.py
+++ DZIEN_3/moje_moduly/main.py
@@ -3,6 +3,12 @@
 from dane import nrfilii as nf
 from dane import book as bk
 
+from funkcje.bfunkcje import czytaj_liste,czytaj_slownik
+
 print("______ dane bezpośrednie ______")
 print(nf)
 print(bk)
+
+print("\n______ wyświetlanie z użyciem funkcji ______")
+czytaj_liste(nf)
+czytaj_slownik(bk)
