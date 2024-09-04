5f79e3ace35a06c23f2ea0cd2ab5e9a2dab94f58
albim72 <marcin.albiniak72@gmail.com>
2024-09-04 14:21:09 +0200

    Update main.py

--- DZIEN_3/moje_moduly/main.py
+++ DZIEN_3/moje_moduly/main.py
@@ -5,6 +5,8 @@ from dane import book as bk
 
 from funkcje.bfunkcje import czytaj_liste,czytaj_slownik
 
+from klasy.filia import Filia
+
 print("______ dane bezpośrednie ______")
 print(nf)
 print(bk)
@@ -12,3 +14,7 @@ print(bk)
 print("\n______ wyświetlanie z użyciem funkcji ______")
 czytaj_liste(nf)
 czytaj_slownik(bk)
+
+print("\n______ wyświetlanie z użyciem obiektu ______")
+f = Filia(6,"ABC","Złota 6, Kraków")
+f.promocja(bk)
