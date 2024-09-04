d3d91d09a8e9fcf7b202d49f1e04d56b1d44dbe9
albim72 <marcin.albiniak72@gmail.com>
2024-09-04 12:05:43 +0200

    Create zadanie_obiekty.py

---
+++ DZIEN_3/zadanie_obiekty.py
@@ -0,0 +1,17 @@
+class Zawodnik:
+    def __init__(self, imie, nazwisko, dyscyplina, wynik):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.dyscyplina = dyscyplina
+        self.wynik = wynik
+
+    def przedstaw_siebie(self):
+        return f"{self.imie} {self.nazwisko}, dyscyplina: {self.dyscyplina}, wynik: {self.wynik}"
+
+# Tworzenie dwóch obiektów klasy Zawodnik
+zawodnik1 = Zawodnik("Anna", "Kowalska", "Lekkoatletyka", 12.34)
+zawodnik2 = Zawodnik("Jan", "Nowak", "Pływanie", 54.21)
+
+# Wyświetlanie przedstawień zawodników
+print(zawodnik1.przedstaw_siebie())
+print(zawodnik2.przedstaw_siebie())
