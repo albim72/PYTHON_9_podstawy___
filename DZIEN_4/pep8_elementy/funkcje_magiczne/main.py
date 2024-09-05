81f0127f89a0d80fcd05f357591797686f1c4336
albim72 <marcin.albiniak72@gmail.com>
2024-09-05 12:02:43 +0200

    Update main.py

--- DZIEN_4/pep8_elementy/funkcje_magiczne/main.py
+++ DZIEN_4/pep8_elementy/funkcje_magiczne/main.py
@@ -1,4 +1,5 @@
 import math
+from singleton import Singleton
 
 if __name__ == '__main__':
     #klasa jednoinstancyjna math
@@ -13,5 +14,16 @@ if __name__ == '__main__':
     # print(id(m2))
     #
     # print(m1 is m2)
-    
-    
+
+    #test działania klasy Singleton
+    ob1 = Singleton()
+    ob2 = Singleton()
+
+    print(ob1 is ob2)
+
+    ob1.dane = "Zmieniono dane!"
+
+    print(f'dane obiekt ob1 -> {ob1.dane}')
+    print(f'dane obiekt ob2 -> {ob2.dane}')
+
+
