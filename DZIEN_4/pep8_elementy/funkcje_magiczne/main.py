05c1af23a904dae6effcfc72be02135126333374
albim72 <marcin.albiniak72@gmail.com>
2024-09-05 11:53:36 +0200

    Create main.py

---
+++ DZIEN_4/pep8_elementy/funkcje_magiczne/main.py
@@ -0,0 +1,17 @@
+import math
+
+if __name__ == '__main__':
+    #klasa jednoinstancyjna math
+    m1 = math
+    m11 = m1.sin(4)
+    print(m11)
+    print(id(m1))
+
+    # m2 = math
+    m12 = m1.pi
+    print(m12)
+    # print(id(m2))
+    #
+    # print(m1 is m2)
+    
+    
