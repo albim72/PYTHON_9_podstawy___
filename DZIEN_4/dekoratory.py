5b2ff7cf3ecea9bf513e094ee635d65f3d6a17f9
albim72 <marcin.albiniak72@gmail.com>
2024-09-05 10:12:12 +0200

    Update dekoratory.py

--- DZIEN_4/dekoratory.py
+++ DZIEN_4/dekoratory.py
@@ -1,3 +1,4 @@
+#funckja wyższego rzędu
 def message(msg):
     return f"pobrana informacja: {msg}"
 
@@ -9,3 +10,26 @@ def infoset(funkcja,*args):
 
 print(infoset(message,"tysiące liczb - 745847583745837458"))
 print(infoset(multiinfo,"mnożenie dwóch wartości",56,7))
+
+#funkcja dekorująca
+
+def startstop(funkcja):
+    def wrapper(*args):
+        print("startowanie procesu....")
+        funkcja(*args)
+        print("kończenie procesu...")
+    return wrapper
+
+def zawijanie(czego):
+    print(f"zawijanie {czego} w sreberka!")
+
+zawijanie("ciastek")
+
+zw = startstop(zawijanie)
+zw("czekoladek")
+
+@startstop
+def dmuchanie(czego):
+    print(f"dmuchanie {czego} na urodzinowym torcie!")
+
+dmuchanie("świeczek")
