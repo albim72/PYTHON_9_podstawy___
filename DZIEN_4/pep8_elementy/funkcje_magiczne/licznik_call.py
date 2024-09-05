48a719de18e7d0c2a7cf707ffa398b93468b6109
albim72 <marcin.albiniak72@gmail.com>
2024-09-05 12:11:52 +0200

    Update licznik_call.py

--- DZIEN_4/pep8_elementy/funkcje_magiczne/licznik_call.py
+++ DZIEN_4/pep8_elementy/funkcje_magiczne/licznik_call.py
@@ -6,3 +6,14 @@ class Licznikwywolan:
         """Zwiększa licznik i wykonuje zadaną akcję..."""
         self.liczba_wywolan += 1
         print(f"funkcja została wywołana {self.liczba_wywolan} razy")
+
+
+class FunkcjaLiniowa:
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+
+    def __call__(self, x):
+        """zwraca wynik funkcji liniowek y = ax + b"""
+        return self.a * x + self.b
+    
