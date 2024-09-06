6289ca7329eaf6e3cfd9967b53674c9326dd8a92
albim72 <marcin.albiniak72@gmail.com>
2024-09-06 11:50:11 +0200

    Update read_xml.py

--- DZIEN_5/read_xml.py
+++ DZIEN_5/read_xml.py
@@ -7,5 +7,10 @@ try:
     for child in root:
         print(f'tag: {child.tag}, atrybyuty: {child.attrib}')
 
+    print("wybrane elementy: ")
+    print(root[0])
+    print(root[0][1])
+    print(root[0][1].text)
+
 except Exception as exc:
     print(exc)
